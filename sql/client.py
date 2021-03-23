c008a8b6734e3fd8e2246ebcc45bc060b16f573b
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-03-22 23:25:09 -0400

    return it

--- sql/client.py
+++ sql/client.py
@@ -33,7 +33,7 @@ class AsyncSQLiteClient:
             self.options = ChangeDict({k: json.loads(v) async for k, v in cursor})
 
     def __await__(self):
-        self.conn.__await__()
+        return self.conn.__await__()
 
     def get(self, user_id: int):
         return self.cache.setdefault(user_id, User(user_id, 0))
